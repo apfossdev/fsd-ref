28270ef525cf1d071232163aa5a323d969999ce8
Annamalai Prabu <apfossdev@gmail.com>
2024-10-08 19:32:15 +0530

    Finish login functionality

--- authentication/todo-with-auth/index.js
+++ authentication/todo-with-auth/index.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const port = 3000;
 
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "iloveneha";
+
 const app = express();
 app.use(express.json());
 
 const users = []; //our super amazing db for now
+
+//auth middleware to verify each request using the token in local storage
+
+function authMiddleware(req, res, next){
+    const token = localStorage.getItem('token');
+    if (!token) {
+      return res.status(400).json({ message: "Token not provided!" });
+    }
+
+    const decodedData = jwt.verify(token, JWT_SECRET);
+
+    if(decodedData.username){
+      req.username = decodedData.username; //to send the username to the next routes through the req by updating the req object
+      res.status(200).json({ message: "Authentication successful" });
+      next();
+    }
+    else{
+        res.status(401).json({ message: "You are not logged in!" });
+    }
+
+}
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
@@ -39,6 +63,50 @@ app.post('/signup', (req, res) => {
           message: "Username already exists!",
         });
     }
+});
+
+app.post('/signin', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  //to check if username and password exist in db and are correct
+
+  //to check if user exists
+  const foundUser = users.find((u) => {
+    if (u.username == username) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  
+  // if username doesn't exist, respond user doesn't exist
+  if (!foundUser) {
+    res.status(400).json({
+      message: "User doesn't exist!",
+    });
+  }
+
+  //if pass correct, generate jwt and store it in local storage
+  else {
+    if (foundUser.password == password) {
+        const token = jwt.sign({
+            username
+        }, JWT_SECRET);
+        res.status(200).json({
+            message: "You are logged in successfully!",
+            token: token
+        });
+    }
+    //else if username exists, password wrong, respond wrong password
+    else{
+        res.status(400).json({
+            message: "Wrong password! Try again."
+        })
+    }
+  } 
+  
 });
 
 
