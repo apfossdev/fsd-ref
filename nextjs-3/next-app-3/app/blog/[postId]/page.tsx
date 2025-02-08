bac7cd75c582e044c1eed4fe88a761f225a66ead
Annamalai Prabu <apfossdev@gmail.com>
2025-02-08 06:54:24 +0530

    Learn Dynamic Routes

---
+++ nextjs-3/next-app-3/app/blog/[postId]/page.tsx
@@ -0,0 +1,21 @@
+// /blog/1
+// /blog/2 these will get handled here
+
+import axios from "axios";
+
+export default async function BlogPage({ params }: any) {
+  const blogId = (await params).postId;
+  const response = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts/${blogId}`
+  );
+  const data = response.data;
+
+  return (
+    <div>
+      Blog page {blogId}
+      <br />
+      title - {data.title}
+      body - {data.body}
+    </div>
+  );
+}
